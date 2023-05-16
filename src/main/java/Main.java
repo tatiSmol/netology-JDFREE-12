b99645c16a3263276d2d6f87f295b68c875ab78f
tatiSmol <judeclapton@icloud.com>
2023-05-17 01:58:13 +0300

    Redefining path to save photo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpEntity;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -40,10 +40,16 @@ public class Main {
         String[] arr = nasaObject.getUrl().split("/");
         String fileName = arr[arr.length - 1];
 
-        // Сохранение в файл
-        HttpEntity entity = pictureResponse.getEntity();
-        try (FileOutputStream fos = new FileOutputStream(fileName)) {
-            entity.writeTo(fos);
+        // Определяем папку для сохранения картинок. Если её нет, то создаём.
+        File folder = new File("nasaPictures");
+        File file = new File(folder, fileName);
+        if (!folder.exists() && !folder.mkdir()) {
+            throw new IOException("Не удалось создать папку для сохранения картинок.");
+        }
+
+        // Сохранение файла
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            pictureResponse.getEntity().writeTo(fos);
         }
 
         httpClient.close();
