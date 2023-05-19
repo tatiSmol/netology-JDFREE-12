270a9011e82601df9b1aae21246dc22cd4c9137f
tatiSmol <judeclapton@icloud.com>
2023-05-20 01:27:16 +0300

    The command "/about" was changed to "/start" for more correct work of the bot

--- src/main/java/NasaTelegramBot.java
+++ src/main/java/NasaTelegramBot.java
@@ -24,7 +24,7 @@ public class NasaTelegramBot extends TelegramLongPollingBot {
         if (update.hasMessage()) {
             chat_id = update.getMessage().getChatId();
             switch (update.getMessage().getText()) {
-                case "/about":
+                case "/start":
                     sendMessage("Привет, я бот NASA! Я высылаю ссылки на картинки по запросу. " +
                             "Напоминаю, что картинки на сайте NASA обновляются раз в сутки.");
                     break;
